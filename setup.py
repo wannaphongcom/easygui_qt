5a7f57131baf3718f1c2dbf8a5a8a68249c450a7
wannaphongcom <wannaphong@yahoo.com>
2016-01-10 12:52:12 +0700

    add qtpy

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 requirements = [
-    # TODO: put package requirements here
+    'qtpy',# TODO: put package requirements here
 ]
 
 test_requirements = [
@@ -33,7 +33,7 @@ setup(
     package_dir={'easygui_qt':
                  'easygui_qt'},
     include_package_data=True,
-    install_requires='',
+    install_requires=requirements,
     license="BSD",
     zip_safe=False,
     keywords='easygui_qt',
